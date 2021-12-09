a8fdb32d86944cda83668f41e1eebfbc94a49b24
Sam Laird <slaird@wpi.edu>
2021-12-09 16:37:33 -0500

    Added automatic handling of setting up keys.json

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const api = require("./keys.json");
 const fs = require('fs');
 
+try {
+    require("./keys.json");
+} catch {
+    const readline = require('readline-sync');
+
+    if (readline.keyInYN("Could not load keys.json. Would you like to setup keys now?")){
+        const token = readline.question("Please enter your discord bot token: ");
+        fs.writeFileSync("./keys.json", JSON.stringify({discordToken: token}, null, 2));
+    } else {
+        console.log("Please create a keys.json file with the following structure:");
+        console.log(JSON.stringify({discordToken: "tokenString"}, null, 2));
+        return 0;
+    }
+}
+const api = require("./keys.json");
+
 const Discord = require('discord.js');
-const bIntents = new Discord.Intents();
-const client = new Discord.Client({ intents: ["DIRECT_MESSAGES", "GUILDS", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGES", "GUILD_EMOJIS_AND_STICKERS", ""], partials: ["CHANNEL"] });
+
+const intents = {
+    intents: [
+        "DIRECT_MESSAGES",
+        "GUILDS",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGES",
+        "GUILD_EMOJIS_AND_STICKERS"],
+    partials: ["CHANNEL"]
+};
+const client = new Discord.Client(intents);
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
